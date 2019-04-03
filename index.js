60ce553ede6ab84315ced51a9303ce2091662ad0
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-03 17:08:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var katzDeliLine = [];
+let i = 1
 
-function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`; 
+function takeANumber(katzDeliLine){
+  katzDeliLine.push(i);
+  return `Welcome, you are number ${i++}.`; 
 }
+
+console.log(takeANumber(katzDeliLine))
+console.log(takeANumber(katzDeliLine))
+console.log(takeANumber(katzDeliLine))
+
 
 function nowServing(katzDeliLine){
   if(!katzDeliLine.length){
@@ -12,13 +18,20 @@ function nowServing(katzDeliLine){
   return `Currently serving ${katzDeliLine.shift()}.`;
 }
 
+
+
 function currentLine(katzDeliLine){
   if(!katzDeliLine.length){
-    return 'The line is currently empty.'
+    return 'The line is currently empty.';
   }
-  var numName = []
+  
+  var numName = [];
+  
   for(let i=0; i < katzDeliLine.length; i++){
-    numName.push(`${i+1}. ${katzDeliLine[i]}`)
+    numName.push(`${i+1}. ${katzDeliLine[i]}`);
   }
-  return `The line is currently: ${numName.join(', ')}`
+  return `The line is currently: ${numName.join(', ')}`;
 }
+
+console.log(currentLine(["Iselin", "Alex"]))
+console.log(currentLine(["Älicia", "Johann"]))
