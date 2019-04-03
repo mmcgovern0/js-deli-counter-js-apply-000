67ad6fdb97ba371774d75bcaaff0cc3b9242a802
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-03 17:11:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,41 +1,10 @@
-var katzDeliLine = [];
-let i = 1
+var katzDeliLine = []
 
-function takeANumber(katzDeliLine){
-  katzDeliLine.push(i);
-  return `Welcome, you are number ${i++}.`; 
-}
-
-console.log(takeANumber(katzDeliLine))
-console.log(takeANumber(katzDeliLine))
-console.log(takeANumber(katzDeliLine))
-
-
-function nowServing(katzDeliLine){
-  if(!katzDeliLine.length){
-    return "There is nobody waiting to be served!";
+function takeANumber(katzDeliLine, name){
+  if(katzDeliLine.length > 0){
+    return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
   }
-  return `Currently serving ${katzDeliLine.shift()}.`;
 }
-
-
-
-function currentLine(katzDeliLine){
-  if(!katzDeliLine.length){
-    return 'The line is currently empty.';
-  }
-  
-  var numName = [];
-  
-  for(let i=0; i < katzDeliLine.length; i++){
-    numName.push(`${i+1}. ${katzDeliLine[i]}`);
-  }
-  return `The line is currently: ${numName.join(', ')}`;
-}
-
-console.log(currentLine(["Iselin", "Alex"]))
-console.log(currentLine(["Älicia", "Johann"]))
-
 
 
 
