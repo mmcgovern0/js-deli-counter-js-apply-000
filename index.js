a4d9454364e3056b87db028372328cdbd4e36ad9
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-03 17:12:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var katzDeliLine = []
 
 function takeANumber(katzDeliLine, name){
+  katzDeliLine.push
   if(katzDeliLine.length > 0){
     return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
   }
