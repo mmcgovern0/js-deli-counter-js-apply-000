b94cbd1b06e1d693fd09180c8304620ab5554c1e
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-03 17:15:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function takeANumber(katzDeliLine, name){
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0){
-    return `
+    return `Currently serving ${katzDeliLine.
   }
 }
 
