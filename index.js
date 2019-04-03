4e430fd43b40e20134f882788e96b074c68c468c
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-03 17:32:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,13 +10,14 @@ function takeANumber(katzDeliLine, name){
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
+console.log(takeANumber(katzDeliLine))
+
 function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0){
     return `Currently serving ${katzDeliLine.shift()}.`
   }
   return "There is nobody waiting to be served!"
 }
-
 
 
 function currentLine(katzDeliLine){
